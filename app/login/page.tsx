db78e68c39472c6ae8f9f3fe3524aa776a21fa43
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-06-04 00:14:21 -0300

    libelula

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,9 @@
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
-
-const LoginForm = dynamic(() => import("./LoginForm"), { ssr: false });
+import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-gemini-gray-50">
-      <Suspense fallback={<div>Carregando...</div>}>
-        <LoginForm />
-      </Suspense>
+      <LoginForm />
     </main>
   );
 }
